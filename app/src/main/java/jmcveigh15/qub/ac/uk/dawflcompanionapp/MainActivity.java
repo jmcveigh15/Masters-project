b36b9af99c7c422aab20e4b44a8f7ae427fe199d
James Mc Veigh <jmcveigh15@qub.ac.uk>
2019-03-15 16:29:48 +0000

    Added basic SMS sending feature

--- app/src/main/java/jmcveigh15/qub/ac/uk/dawflcompanionapp/MainActivity.java
+++ app/src/main/java/jmcveigh15/qub/ac/uk/dawflcompanionapp/MainActivity.java
@@ -1,14 +1,32 @@
 package jmcveigh15.qub.ac.uk.dawflcompanionapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSendResultsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // this sets the send result button
+        // only users logged in as team admin can see this button
+        mSendResultsButton = (Button) findViewById(R.id.send_result);
+        mSendResultsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_TEXT, getString(R.string.result_instructions));
+                i = Intent.createChooser(i, getString(R.string.send_result));
+                startActivity(i);
+            }
+        });
 
     }
 }
